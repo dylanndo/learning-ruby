1e810e8fcb35c0a69c038f5f8fce504423a9c53f
Dylan Do <dylan2002do@yahoo.com>
2023-05-25 19:33:48 -0400

    test puts and print

--- dylan.rb
+++ dylan.rb
@@ -1,1 +1,6 @@
-print "Hello World"
+# Difference between puts and print:
+# puts adds a new line after executing
+# print does not add a new line after executing
+
+# print "Hello World"
+# puts "Roadie lit"
