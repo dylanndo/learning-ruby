986212b8d599bf232fdead6a283c379de65b28b6
Dylan Do <dylan2002do@yahoo.com>
2023-05-26 02:21:31 -0400

    uncomment print and put statements

--- dylan.rb
+++ dylan.rb
@@ -2,10 +2,10 @@
 # puts adds a new line after executing
 # print does not add a new line after executing
 
-# print "Hello World"
-# print "lol"
-# puts "Roadie lit"
-# puts "i'm a roadie intern"
+print "Hello World"
+print "lol"
+puts "Roadie lit"
+puts "i'm a roadie intern"
 
 # drawing basic shapes
 puts "   /|"
