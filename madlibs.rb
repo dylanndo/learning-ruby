407fba769a1bda6a52a10694e8a0d511814aad88
dylanndo <62081958+dylanndo@users.noreply.github.com>
2023-05-30 15:14:45 -0400

    make all user words uppercase

--- madlibs.rb
+++ madlibs.rb
@@ -1,19 +1,19 @@
 # sample mad lib from https://kidscodecs.com/python-mad-libs/#:~:text=AN%20EXAMPLE%20MADLIB&text=It%20was%20pizza%20day%20at,and%20ran%20through%20the%20playground.
 
 puts "Enter a food: "
-food = gets.chomp()
+food = gets.chomp().upcase()
 puts "Enter a girl's name: "
-name = gets.chomp()
+name = gets.chomp().upcase()
 puts "Enter an adjective: "
-adjective = gets.chomp()
+adjective = gets.chomp().upcase()
 puts "Enter a noun: "
-noun = gets.chomp()
+noun = gets.chomp().upcase()
 puts "Enter a verb: "
-verb1 = gets.chomp()
+verb1 = gets.chomp().upcase()
 puts "Enter another verb: "
-verb2 = gets.chomp()
+verb2 = gets.chomp().upcase()
 puts "Enter another verb: "
-verb3 = gets.chomp()
+verb3 = gets.chomp().upcase()
 
 puts ("It was " + food + " day at school, and " + name + " was super " + adjective + " for lunch.")
 puts ("But when she went outside to eat, a " + noun + " stole her " + food + "! ")
